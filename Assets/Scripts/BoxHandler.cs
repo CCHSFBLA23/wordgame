42d68d498f3ff46df1a0eb066e8fc79fbef255c0
Gil <gilpowers2006@gmail.com>
2022-09-17 15:14:38 -0500

    fixed the stupid linked stuff

--- Assets/Scripts/BoxHandler.cs
+++ Assets/Scripts/BoxHandler.cs
@@ -22,6 +22,7 @@ public class BoxHandler : MonoBehaviour
     [HideInInspector]public Box[] boxes;
     private List<Box> _falling = new List<Box>();
     private List<Box> _linked = new List<Box>();
+    public List<Box> _toMoveLinked = new List<Box>();
 
     private LevelHandler _levelHandler;
     [HideInInspector]public GridPosition playerPosition;
@@ -42,16 +43,6 @@ public class BoxHandler : MonoBehaviour
         for (int i = 0; i < boxObjects.Length; i++)
         {
             boxes[i] = boxObjects[i].GetComponent<Box>();
-            //Sets starting positions for each of the boxes.
-            //Adds boxes of type in to list.
-            if (boxes[i].falling)
-            {
-                _falling.Add(boxes[i]);
-            }
-            if (boxes[i].linked)
-            {
-                _linked.Add(boxes[i]);
-            }
         }
 
         _levelHandler = GetComponent<LevelHandler>();
@@ -95,10 +86,12 @@ public class BoxHandler : MonoBehaviour
     private bool PushRowOfBoxes(Vector2 position, Vector2 moveVector)
     {
         Box cur;
-        var toPush = new List<GridPosition>();
+        Box starting;
+        var toPush = new List<Box>();
         if (CheckBoxCollision(position, moveVector))
         {
             cur = CheckBoxCollision(position, moveVector);
+            starting = cur;
             if (!cur.pushable) return false;
             toPush.Add(cur);
         }
@@ -111,112 +104,141 @@ public class BoxHandler : MonoBehaviour
         {
             cur = CheckBoxCollision(cur.target, moveVector);
             if (!cur.pushable) return false;
+            if (cur.target == playerPosition.target) return false;
             toPush.Add(cur);
         }
-        
+
         if (CheckWallCollisions(cur.target, moveVector))
         {
             return false;
         }
         else
         {
+            bool moved = false;
             foreach (var gridPosition in toPush)
+            {
+                if (gridPosition.linked && !gridPosition.linkedMoved)
+                {
+                    gridPosition.linkedMoved = true;
+                    foreach (var box in _linked)
+                    {
+                        if (!toPush.Contains(box) && !moved && !box.linkedMoved)
+                        {
+                            box.linkedMoved = true;
+                            PushRowOfBoxes(box.target - moveVector, moveVector);
+                        }
+                    }
+                    moved = true;
+                }
+                gridPosition.target += moveVector;
+            }
+            
+        }
+
+        return true;
+    }
+
+    //Pushes in negative move vector basically.
+        //See PushRowOfBoxes for a similar process.
+        void PullRowOfBoxes(Vector2 position, Vector2 moveVector)
+        {
+            Box cur;
+            var toPull = new List<GridPosition>();
+
+            if (!CheckBoxCollision(position, -moveVector)) return;
+            cur = CheckBoxCollision(position, -moveVector);
+            if (!cur.pullable) return;
+            toPull.Add(cur);
+        
+            while (CheckBoxCollision(cur.target, -moveVector))
+            {
+                cur = CheckBoxCollision(cur.target, -moveVector);
+                if (!cur.pullable) break;
+                
+                toPull.Add(cur);
+            }
+
+            foreach (var gridPosition in toPull)
             {
                 gridPosition.target += moveVector;
             }
-            return true;
-        }
-    }
-    //Pushes in negative move vector basically.
-    //See PushRowOfBoxes for a similar process.
-    void PullRowOfBoxes(Vector2 position, Vector2 moveVector)
-    {
-        Box cur;
-        var toPull = new List<GridPosition>();
-
-        if (!CheckBoxCollision(position, -moveVector)) return;
-        cur = CheckBoxCollision(position, -moveVector);
-        if (!cur.pullable) return;
-        toPull.Add(cur);
-        
-        while (CheckBoxCollision(cur.target, -moveVector))
-        {
-            cur = CheckBoxCollision(cur.target, -moveVector);
-            if (!cur.pullable) break;
-            toPull.Add(cur);
         }
 
-        foreach (var gridPosition in toPull)
+        //Deals with sending input to the player target (aka preventing it from jumping around).
+        //Checks walls and the box pushing stuff above.
+        private void CalculateMovementPlayer()
         {
-            gridPosition.target += moveVector;
-        }
-    }
-
-    //Deals with sending input to the player target (aka preventing it from jumping around).
-    //Checks walls and the box pushing stuff above.
-    private void CalculateMovementPlayer()
-    {
-        if (!(Vector2.Distance(playerPosition.current, playerPosition.target) <= .05f) || _inputVector == Vector2.zero) return;
+            if (!(Vector2.Distance(playerPosition.current, playerPosition.target) <= .05f) || _inputVector == Vector2.zero) return;
         
-        var moveVector = CalcMoveVector(_inputVector);
+            var moveVector = CalcMoveVector(_inputVector);
         
-        var canMovePlayer = true;
-        if (CheckBoxCollision(playerPosition.target, moveVector))
-        {
-            //Checks if the original box pushed is a linked box.
-            var originalBox = CheckBoxCollision(playerPosition.target, moveVector);
-            canMovePlayer = PushRowOfBoxes(playerPosition.target, moveVector);
-            //todo fix because broken!!!!!! for now make it so there are only two linked per screen until fixed.
-            if (originalBox.linked && canMovePlayer)
+            var canMovePlayer = true;
+            if (CheckBoxCollision(playerPosition.target, moveVector))
             {
-                foreach (var box in _linked.Where(box => box != originalBox))
+                //Checks if the original box pushed is a linked box.
+                var originalBox = CheckBoxCollision(playerPosition.target, moveVector);
+                canMovePlayer = PushRowOfBoxes(playerPosition.target, moveVector);
+            }
+
+            if (!CheckWallCollisions(playerPosition.target, moveVector) && canMovePlayer)
+            {
+                PullRowOfBoxes(playerPosition.target, moveVector);
+                playerPosition.target += moveVector;
+            }
+        }
+        private void CalculateFallingMovement()
+        {
+            foreach (var box in _falling)
+            {
+            
+                if(Vector2.Distance(box.target, box.current) > 0.1f) return;
+                Vector2 cur = box.target;
+                if (cur + Vector2.down == playerPosition.target) return;
+            
+                while (true)
                 {
-                    PushRowOfBoxes(box.target - moveVector, moveVector);
+                
+                    if (CheckWallCollisions(cur, Vector2.down)) break;
+                    if (CheckBoxCollision(cur, Vector2.down)) break;
+                    if (playerPosition.target == cur + Vector2.down) break;
+                
+                    cur += Vector2.down;
+                }
+                box.target = cur;
+            }
+        }
+        private Vector2 CalcMoveVector(Vector2 input)
+        {
+            Vector2 moveVector = new Vector2();
+            if (input.x != 0 ^ input.y != 0)
+            {
+                moveVector = input;
+            }
+            return moveVector;
+        }
+    
+        private void Update()
+        {
+            _falling = new List<Box>();
+            _linked = new List<Box>();
+            
+            foreach (var box in boxes)
+            {
+                if (box.falling)
+                {
+                    _falling.Add(box);
+                }
+
+                if (box.linked)
+                {
+                    box.linkedMoved = false;
+                    _linked.Add(box);
                 }
             }
-        }
 
-        if (!CheckWallCollisions(playerPosition.target, moveVector) && canMovePlayer)
-        {
-            PullRowOfBoxes(playerPosition.target, moveVector);
-            playerPosition.target += moveVector;
+            _toMoveLinked = _linked;
+            CalculateMovementPlayer();
+            CalculateFallingMovement();
         }
-    }
-    private void CalculateFallingMovement()
-    {
-        foreach (var box in _falling)
-        {
-            
-            if(Vector2.Distance(box.target, box.current) > 0.1f) return;
-            Vector2 cur = box.target;
-            if (cur + Vector2.down == playerPosition.target) return;
-            
-            while (true)
-            {
-                
-                if (CheckWallCollisions(cur, Vector2.down)) break;
-                if (CheckBoxCollision(cur, Vector2.down)) break;
-                if (playerPosition.target == cur + Vector2.down) break;
-                
-                cur += Vector2.down;
-            }
-            box.target = cur;
-        }
-    }
-    private Vector2 CalcMoveVector(Vector2 input)
-    {
-        Vector2 moveVector = new Vector2();
-        if (input.x != 0 ^ input.y != 0)
-        {
-            moveVector = input;
-        }
-        return moveVector;
-    }
-    
-    private void Update()
-    {
-        CalculateMovementPlayer();
-        CalculateFallingMovement();
-    }
 
 }
